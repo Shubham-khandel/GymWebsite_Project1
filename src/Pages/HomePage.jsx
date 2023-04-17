6f9de14b656c257f97beef17c63ac836d163281c
sandeshghadage <sandeshghadage3@gmail.com>
2023-04-17 23:06:01 +0530

    curousel

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import TrainingPrograms from "./TrainigProgramSection/TrainingPrograms";
 
 function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
+      <h1 id="about">About Us</h1>
+      <TrainingPrograms />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
---
+++ src/Pages/TrainigProgramSection/TrainingPrograms.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import styles from "./TrainingPrograms.module.css";
+
+export default function TrainingPrograms() {
+  const trainingData = [
+    {
+      type: "Yoga",
+      imgUrl:
+        "https://images.pexels.com/photos/2597205/pexels-photo-2597205.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      type: "Body Building",
+      imgUrl:
+        "https://images.pexels.com/photos/3289711/pexels-photo-3289711.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      type: "Aerobic",
+      imgUrl:
+        "https://media.istockphoto.com/id/618730140/photo/group-training-on-steppers.jpg?b=1&s=170667a&w=0&k=20&c=UzdGCz5s2E2MkJM1mdRnn2WjtVC7gDX89VX2RM1WKbM=",
+    },
+    {
+      type: "Flexibility",
+      imgUrl:
+        "https://images.pexels.com/photos/4057112/pexels-photo-4057112.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      type: "Zumba",
+      imgUrl:
+        "https://media.istockphoto.com/id/1441559219/photo/zumba-fitness.jpg?b=1&s=612x612&w=0&k=20&c=FcwUo56FpgMQljhNF9ijmL_YKS9plZLZ6RvPdAvHJv0=",
+    },
+    {
+      type: "Yoga",
+      imgUrl:
+        "https://images.pexels.com/photos/2597205/pexels-photo-2597205.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      type: "Body Building",
+      imgUrl:
+        "https://images.pexels.com/photos/3289711/pexels-photo-3289711.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  ];
+
+  return (
+    <div className={styles.carouselContainer}>
+      <div id={styles.carouselText}>Training Programs</div>
+      <div className={styles.carousel}>
+        {trainingData.map((item, index) => {
+          return (
+            <TrainingTypeCard
+              type={trainingData[index].type}
+              imgUrl={trainingData[index].imgUrl}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+function TrainingTypeCard({ type, imgUrl }) {
+  return (
+    <div className={styles.cardBox}>
+      <img className={styles.card} src={imgUrl}></img>
+      <div id={styles.cardTypeText}>{type}</div>
+    </div>
+  );
+}
