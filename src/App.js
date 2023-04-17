553d9e0e2092a65cfde54aa034ad870bae7a37ff
Shubham-Sharma <viku.vsk@gmail.com>
2023-04-17 13:45:07 +0530

    InitialRouting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
+import AboutUs from "./Pages/AboutUs";
+import NavBar from "./Pages/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <NavBar />
+      <div className="App"></div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+      </Routes>
+    </>
   );
 }
 
