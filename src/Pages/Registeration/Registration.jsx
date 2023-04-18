1552c7e5a4200c08a644a1c41aa1f3e3b6491d57
Shubham-Sharma <viku.vsk@gmail.com>
2023-04-18 23:17:44 +0530

    Login_registration

--- src/Pages/Registeration/Registration.jsx
+++ src/Pages/Registeration/Registration.jsx
@@ -10,7 +10,8 @@ export default function Registration() {
   const [details, setDetails] = useState([])
 
   const obj = {name:name,email:email,number:number,password:password}
-
+  // const getData = localStorage.getItem('userdetails')
+  
   function handleSubmit(e){
     e.preventDefault()
     let newData = [...details, obj]
@@ -30,11 +31,15 @@ export default function Registration() {
       <div className={Styles.innerContainer} >
       <form type='submit'>
         <h1>Register Here</h1>
+        <span>Name:</span>
         <input value={name} onChange={(e)=>setName(e.target.value)} placeholder='Full Name' type='text'/>
+        <span>Email:</span>
         <input value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Email' type='email'/>
+        <span>Phone Number:</span>
         <input value={number} onChange={(e)=>setNumber(e.target.value)} placeholder='Phone Number' type='number'/>
+        <span>Enter Password:</span>
         <input value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Enter password' type='password'/>
-        <span>Already a member? <Link to='/login'>Login here</Link></span>        
+        <span className={Styles.loginBtn}>Already a member? <Link to='/login'>Login here</Link></span>        
         <button className={Styles.submitButton} onClick={handleSubmit}>Submit</button>
       </form>
       </div>
