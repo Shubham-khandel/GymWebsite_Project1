42123f7e4c6f2123a94a664edcd8a6d6bdda13c0
santosh4421 <santoshdaberao1610@mail.com>
2023-04-19 20:11:20 +0530

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import style from "./Footer.module.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { FiCamera } from "react-icons/fi";
 import { FiTwitter } from "react-icons/fi";
 import { FaFacebookF } from "react-icons/fa";
@@ -12,11 +12,11 @@ function Footer() {
     <footer>
       <div className={style.main}>
         <div className={style.main1}>
-          <h1 style={{ color: "black" }} >ONYX</h1>
-          <p style={{padding:"0.2rem"}} >500 Terry Francine</p>
-          <p style={{padding:"0.2rem"}}>Street San Francisco, CA</p>
-          <p style={{padding:"0.2rem"}}>94158</p>
-          <p style={{padding:"0.2rem"}}>123-456-7890</p>
+          <h1 style={{ color: "black" }}>ONYX</h1>
+          <p style={{ padding: "0.2rem" }}>500 Terry Francine</p>
+          <p style={{ padding: "0.2rem" }}>Street San Francisco, CA</p>
+          <p style={{ padding: "0.2rem" }}>94158</p>
+          <p style={{ padding: "0.2rem" }}>123-456-7890</p>
           <div className={style.icons}>
             <FiCamera id={style.icon1} />
             <FaFacebookF id={style.icon2} />
@@ -28,30 +28,30 @@ function Footer() {
         </div>
         <div className={style.main2}>
           <div className={style.main21}>
-          <h3>Not a Member? Get a 45 Day Free-Trial</h3>
-          <p>Fill out the form below and a sales rep will get in touch shortly.</p>
+            <h3>Not a Member? Get a 45 Day Free-Trial</h3>
+            <p>
+              Fill out the form below and a sales rep will get in touch
+              shortly..
+            </p>
           </div>
-           <div className={style.form}>
-            <Form/>
-           </div>
+          <div className={style.form}>
+            <Form />
+          </div>
         </div>
-        
+
         <div className={style.main4}>
-          <h3 style={{padding:"5px"}}>Menu</h3>
+          <h3 style={{ padding: "5px" }}>Menu</h3>
 
-
-         {/* <span><Link to='/' >Home</Link></span><br/> 
+          {/* <span><Link to='/' >Home</Link></span><br/> 
          <span> <Link path="/aboutus" >About</Link></span><br/> */}
-         <p style={{ padding:"0.3rem"}}>Home</p>
-         <p style={{ padding:"0.3rem"}}>About</p>
-          <p style={{ padding:"0.3rem"}}>Services</p>
-          <p style={{padding:"0.3rem"}}>Membership</p>
-          <p style={{padding:"0.3rem"}}>Facilities</p>
+          <p style={{ padding: "0.3rem" }}>Home</p>
+          <p style={{ padding: "0.3rem" }}>About</p>
+          <p style={{ padding: "0.3rem" }}>Services</p>
+          <p style={{ padding: "0.3rem" }}>Membership</p>
+          <p style={{ padding: "0.3rem" }}>Facilities</p>
         </div>
-         
       </div>
     </footer>
   );
 }
 export default Footer;
-
