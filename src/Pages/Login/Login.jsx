abd954f1b24876488fffc4c0c8d22314e93ff898
Neha-rajbhar <nr470245@gmail.com>
2023-04-19 20:41:04 +0530

    Stash changes

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,28 +1,24 @@
-import React, { useState } from "react";
-import Styles from "../Registeration/RegestrationForm.module.css";
-import { Link } from "react-router-dom";
+import React, {useState} from 'react'
+import Styles from '../Registeration/RegestrationForm.module.css'
+import {Link} from 'react-router-dom'
 
-export function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  return (
+export function Login(){
+  
+  const registerData = localStorage.getItem('details')
+  console.log(registerData)
+  return(
     <div>
       <div className={Styles.container}>
-        <div className={Styles.innerContainer}>
-          <form type="submit">
-            <h1>Login Here</h1>
-            <span>Email:</span>
-            <input value={email} placeholder="Email" type="email" />
-            <span>Enter Password:</span>
-            <input value={password} placeholder="Password" type="password" />
-            <span>
-              Not a member? <Link to="/register">Register here</Link>
-            </span>
-            <button className={Styles.submitButton}>Submit</button>
-          </form>
-        </div>
+      <div className={Styles.innerContainer} >
+      <form type='submit'>
+        <h1>Login Here</h1>
+        <input  placeholder='Email' type='email'/>
+        <input  placeholder='Password' type='password'/>
+        <span>Not a member? <Link to='/register'>Register here</Link></span>        
+        <button className={Styles.submitButton}>Submit</button>
+      </form>
       </div>
     </div>
-  );
+    </div>
+  )
 }
