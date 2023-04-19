a25f0bb257a8c17e578cf035858adb73f52ace97
santosh4421 <santoshdaberao1610@mail.com>
2023-04-19 17:58:27 +0530

    footer changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,36 +4,49 @@ import { Link } from 'react-router-dom';
 import { FiCamera } from "react-icons/fi";
 import { FiTwitter } from "react-icons/fi";
 import { FaFacebookF } from "react-icons/fa";
+// import Registration from "../Pages/Registeration/Registration";
+import { Form } from "./Form";
+
 function Footer() {
   return (
     <footer>
       <div className={style.main}>
         <div className={style.main1}>
-          <h1 style={{ color: "black" }}>ONYX</h1>
-          <p>500 Terry Francine</p>
-          <p>Street San Francisco, CA</p>
-          <p>94158</p>
-          <p>123-456-7890</p>
+          <h1 style={{ color: "black" }} >ONYX</h1>
+          <p style={{padding:"0.2rem"}} >500 Terry Francine</p>
+          <p style={{padding:"0.2rem"}}>Street San Francisco, CA</p>
+          <p style={{padding:"0.2rem"}}>94158</p>
+          <p style={{padding:"0.2rem"}}>123-456-7890</p>
           <div className={style.icons}>
             <FiCamera id={style.icon1} />
             <FaFacebookF id={style.icon2} />
             <FiTwitter id={style.icon3} />
             {/* <a href="info@mysite.com">info@mysite.com</a> */}
             {/* <p>© 2035 by Onyx. Powered <br/>and secured by Wix</p> */}
+            <p>2035 by Onyx. Powered and secured by Wix</p>
           </div>
         </div>
         <div className={style.main2}>
+          <div className={style.main21}>
           <h3>Not a Member? Get a 45 Day Free-Trial</h3>
-          <p>Fill out the form below and a sales rep <br/>will get in touch shortly.</p>
+          <p>Fill out the form below and a sales rep will get in touch shortly.</p>
+          </div>
+           <div className={style.form}>
+            <Form/>
+           </div>
         </div>
         
         <div className={style.main4}>
           <h3 style={{padding:"5px"}}>Menu</h3>
-         <span><Link to='/' style={{textDecoration:"none",color:"black",padding:"5px"}}>Home</Link></span><br/> 
-         <span> <Link path="/aboutus" style={{textDecoration:"none",color:"black",padding:"5px"}}>About</Link></span><br/>
-          <p style={{ padding:"5px"}}>Services</p>
-          <p style={{padding:"5px"}}>Membership</p>
-          <p style={{padding:"5px"}}>Facilities</p>
+
+
+         {/* <span><Link to='/' >Home</Link></span><br/> 
+         <span> <Link path="/aboutus" >About</Link></span><br/> */}
+         <p style={{ padding:"0.3rem"}}>Home</p>
+         <p style={{ padding:"0.3rem"}}>About</p>
+          <p style={{ padding:"0.3rem"}}>Services</p>
+          <p style={{padding:"0.3rem"}}>Membership</p>
+          <p style={{padding:"0.3rem"}}>Facilities</p>
         </div>
          
       </div>
@@ -41,3 +54,4 @@ function Footer() {
   );
 }
 export default Footer;
+
---
+++ src/Components/Form.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import React from "react";
+import Styles from "./Form.module.css"
+import { Link } from "react-router-dom";
+
+
+export function Form(){
+    const [name , setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [number, setNumber] = useState('');
+    const [password, setPassword] = useState('')
+    const [details, setDetails] = useState([])
+  
+    const obj = {name:name,email:email,number:number,password:password}
+    // const getData = localStorage.getItem('userdetails')
+    
+    function handleSubmit(e){
+      e.preventDefault()
+      let newData = [...details, obj]
+      setDetails(newData);
+      console.log(newData)
+      localStorage.setItem("userdetails",JSON.stringify(newData))
+      setEmail('')
+      setName('')
+      setNumber('')
+      setPassword('')
+    }
+  
+  
+    return (
+  
+      <div className={Styles.container}>
+        <div className={Styles.innerContainer} >
+        <form type='submit'>
+          {/* <h1>Registration Form</h1> */}
+          {/* <span>Name:</span> */}
+          <input value={name} onChange={(e)=>setName(e.target.value)} placeholder='Full Name' type='text'/>
+          {/* <span>Email:</span> */}
+          <input value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Email' type='email'/>
+          {/* <span>Phone Number:</span> */}
+          <input value={number} onChange={(e)=>setNumber(e.target.value)} placeholder='Phone Number' type='number'/>
+          {/* <span>Enter Password:</span> */}
+          <input value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Add a message' type='text'/>
+          {/* <span className={Styles.loginBtn}>Already a member? <Link to='/login'>Login here</Link></span>         */}
+          <button className={Styles.submitButton} onClick={handleSubmit}>Submit</button>
+        </form>
+        </div>
+      </div>
+    );
+  }
