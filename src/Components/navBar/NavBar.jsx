f65963f5e02a7a95ce1ea60f8e8720a5dbf874a8
Neha-rajbhar <nr470245@gmail.com>
2023-04-20 17:56:43 +0530

    Update In HeroImage

--- src/Components/navBar/NavBar.jsx
+++ src/Components/navBar/NavBar.jsx
@@ -1,68 +1,31 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import styles from "./NavBar.module.css";
-import { useState } from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { ImCross } from "react-icons/im";
-import { useNavigate } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './NavBar.module.css'
 
 const NavBar = () => {
-  const [isMobile, setMobile] = useState(false);
-  const navigate=useNavigate();
-  function handleLogin(){
-  let users= JSON.parse(localStorage.getItem("details"));
-  if(users){
-    navigate("/login")
-  }
-  else{
-    navigate("/register")
-  }
-    
-  }
   return (
     <nav className={styles.mainNav}>
       <div className={styles.logo}>
         <h2>GYM</h2>
       </div>
-      <div className={isMobile ? styles.navbarMobile : styles.links}>
+      <div className={styles.links}>
         <ul>
-          <li>
-            <NavLink className={styles.navlink} to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className={styles.navlink} to="/aboutus">
-              About Us
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className={styles.navlink} to="/login">
-              Login
-            </NavLink>
-          </li>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/aboutus'>About Us</Link></li>
+          <li><Link to='/login'>Login</Link></li>
+         
         </ul>
+        
+        
+
       </div>
       <div className={styles.button}>
         <button onClick={handleLogin} className={styles.btn}>Join us</button>
+        <button className={styles.btn}>Join us</button>
       </div>
-      <div className={styles.userProfile}>
-        <div className={styles.menuBtn}>
-          <button onClick={() => setMobile(!isMobile)}>
-            {isMobile ? (
-              <i className="fas fa-times">
-                <ImCross />
-              </i>
-            ) : (
-              <i className="fas fa-bars">
-                <GiHamburgerMenu />
-              </i>
-            )}
-          </button>
-        </div>
-      </div>
-    </nav>
-  );
-};
 
-export default NavBar;
+    </nav>
+  )
+}
+
+export default NavBar
